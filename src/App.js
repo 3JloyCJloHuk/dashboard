4d3ac5068bda71b3dfc9eb703348527656fddc57
Костя <kostyaopanasuk@gmail.com>
2022-10-26 23:25:07 +0300

    day1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "./layouts";
+import Home from "./page/home";
+import Product from "./page/Product";
+import Customers from "./page/Customers";
+import Income from "./page/Income";
+import Promote from "./page/Promote";
+import Help from "./page/Help";
+import NotFoundPage from "./page/NotFoundPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<MainLayout />}>
+        <Route path='' element={<Home />} />
+        <Route path='product' element={<Product />} />
+        <Route path='customers' element={<Customers />} />
+        <Route path='income' element={<Income />} />
+        <Route path='promote' element={<Promote />} />
+        <Route path='help' element={<Help />} />
+      </Route>
+      <Route path='*' element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
