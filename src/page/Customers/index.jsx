f5cf8487eae8661743e29a38267e264f765c33c7
Костя <kostyaopanasuk@gmail.com>
2022-10-27 23:39:28 +0300

    day2

--- src/page/Customers/index.jsx
+++ src/page/Customers/index.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import ReactPaginate from "react-paginate";
 
 const Customers = () => {
+    const [itemOffset, setItemOffset] = React.useState(0);
+
+    const handlePageClick = (event) => {
+        const newOffset = event.selected * 1 % 40;
+        console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
+        setItemOffset(newOffset);
+    };
+
     return (
         <section className="customers">
             <h3 className="customers__greeting">Hello Evano 👋🏼</h3>
@@ -11,7 +20,7 @@ const Customers = () => {
                         <p className="customers__active">Active Members</p>
                     </div>
                     <div className="customers__search">
-                        <label className="customers__search-img" for="search">
+                        <label className="customers__search-img" htmlFor="search">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke="#7E7E7E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                                 <path d="M21 21L16.65 16.65" stroke="#7E7E7E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -20,24 +29,63 @@ const Customers = () => {
                         <input className="customers__search-input" id="search" type="search" placeholder="Search" />
                     </div>
                 </div>
-                <div className="table">
-                    <ul className="table__head">
-                        <li className="table__title">Customer Name</li>
-                        <li className="table__title">Company</li>
-                        <li className="table__title">Phone Number</li>
-                        <li className="table__title">Email</li>
-                        <li className="table__title">Country</li>
-                        <li className="table__title">Status</li>
-                    </ul>
-                    <ul className="table__body">
-                        <li className="table__description">Jane Cooper</li>
-                        <li className="table__description">Microsoft</li>
-                        <li className="table__description">(225) 555-0118</li>
-                        <li className="table__description">jane@microsoft.com</li>
-                        <li className="table__description">United States</li>
-                        <li className="table__description">Active</li>
-                    </ul>
+                <table className="table">
+                    <thead className="table__head">
+                        <tr className="table__head-row">
+                            <th className="table__title">Customer Name</th>
+                            <th className="table__title">Company</th>
+                            <th className="table__title">Phone Number</th>
+                            <th className="table__title">Email</th>
+                            <th className="table__title">Country</th>
+                            <th className="table__title">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody className="table__body">
+                        <tr className="table__body-row">
+                            <td className="table__description">Jane Cooper</td>
+                            <td className="table__description">Microsoft</td>
+                            <td className="table__description">(225) 555-0118</td>
+                            <td className="table__description">jane@microsoft.com</td>
+                            <td className="table__description">United States</td>
+                            <td className="table__description">
+                                <button className="table__description-button table__description-button--active">Active</button>
+                            </td>
+                        </tr>
+                        <tr className="table__body-row">
+                            <td className="table__description">Floyd Miles</td>
+                            <td className="table__description">Yahoo</td>
+                            <td className="table__description">(205) 555-0100</td>
+                            <td className="table__description">floyd@yahoo.com</td>
+                            <td className="table__description">Kiribati</td>
+                            <td className="table__description">
+                                <button className="table__description-button table__description-button--inactive">Inactive</button>
+                            </td>
+                        </tr>
+                    </tbody>
 
+                </table>
+                <div className="paginate">
+                    <h4 className="paginate__info">Showing data 1 to 8 of  256K entries</h4>
+                    <ReactPaginate
+                        nextLabel=">"
+                        onPageChange={handlePageClick}
+                        pageRangeDisplayed={4}
+                        marginPagesDisplayed={1}
+                        pageCount={40}
+                        previousLabel="<"
+                        pageClassName="pagination__item"
+                        pageLinkClassName="pagination__item-link"
+                        previousClassName="pagination__item"
+                        previousLinkClassName="pagination__item-link"
+                        nextClassName="pagination__item"
+                        nextLinkClassName="pagination__item-link"
+                        breakLabel="..."
+                        breakClassName="pagination__item"
+                        breakLinkClassName="pagination__item-link"
+                        containerClassName="pagination"
+                        activeLinkClassName="pagination__item-link pagination__item-link--active"
+                        renderOnZeroPageCount={null}
+                    />
                 </div>
             </div>
         </section>
